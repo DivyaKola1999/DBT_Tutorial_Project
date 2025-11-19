a409d86c31ae7a38950d064e34c12b6d053d9b47
Divya Kola <divya.kola@pwc.com>
2025-11-19 15:15:15 +0000

    c

--- tests/test_product_sales_summary.sql
+++
@@ -1,8 +0,0 @@
--- This test fails if any metric is null or negative
-
-select *
-from {{ ref('product_sales_summary') }}
-where total_quantity_sold <= 0
-   or total_quantity_sold is null
-   or total_sales_amount <= 0
-   or total_sales_amount is null
