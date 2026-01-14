aedb9f4fd3cc2bbc67d8824e66c31295141ff92c
Divya Kola <divya.kola@pwc.com>
2026-01-14 15:31:36 +0000

    c

--- analyses/source_data_analysis.sql
+++ analyses/source_data_analysis.sql
@@ -1,11 +1,17 @@
 with source_data as (
     select
-        customerid,
-        name,
-        age,
-        city,
-        email,
-        updated_at
+        SELECT
+    customerid,
+    name,
+    age,
+    CASE
+        WHEN age < 18 THEN 'Minor'
+        WHEN age BETWEEN 18 AND 34 THEN 'Young Adult'
+        WHEN age BETWEEN 35 AND 54 THEN 'Adult'
+        ELSE 'Senior'
+    END AS age_group,
+    city,
+    email
     from {{ source('dbt_source_data_bronze', 'CUSTOMER') }}
 )
 
